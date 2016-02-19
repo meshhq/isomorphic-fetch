42ee2f59c8a19e36ce13ca6d415717b4fb485a9e
Taylor Halliday <taylor.halliday@gmail.com>
2016-02-18 21:58:43 -0800

    exposed headers, req, resp to browser

--- fetch-npm-browserify.js
+++ fetch-npm-browserify.js
@@ -4,3 +4,6 @@
 // Return that as the export for use in Webpack, Browserify etc.
 require('whatwg-fetch');
 module.exports = self.fetch.bind(self);
+module.exports.Headers = self.Headers;
+module.exports.Request = self.Request;
+module.exports.Response = self.Response;
